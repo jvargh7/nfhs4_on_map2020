40b804988190dd30878459ce89d308225b104b4b
Varghese <JVARGH7@emory.edu>
2022-09-16 18:28:40 -0400

    Added missing psu imputation

--- code/n4d2004_psus within district map 2018.R
+++ code/n4d2004_psus within district map 2018.R
@@ -1,5 +1,8 @@
 # https://gis.stackexchange.com/questions/323698/counting-points-in-polygons-with-sf-package-of-r
-library(sf)
+require(sf)
+require(purrr)
+require(tidyverse)
+
 
 path_nfhs <- "C:/Cloud/OneDrive - Emory University/data/dhs_program/IA/IAGE71FL"
 shp_nfhs <-  read_sf(paste0(path_nfhs))
@@ -21,24 +24,23 @@ shp_nfhs_2018 <- map2_dfr(psu_2018,1:719,
                             
                           })
 
-write_csv(shp_nfhs_2018,paste0("data/psu_on_map2018.csv"))
 
-path_2020 <- "C:/Cloud/OneDrive - Emory University/data/India Shapefiles/india_shp_2020-master/district"
-shp_2020 <-  read_sf(paste0(path_2020))
-psu_2020 <- st_intersects(shp_2020,shp_nfhs)
+missing_shp_nfhs <- anti_join(shp_nfhs %>% 
+                                dplyr::select(one_of("DHSCLUST","ADM1NAME","DHSREGCO","DHSREGNA")),
+                              shp_nfhs_2018,
+                              by=c("DHSCLUST","ADM1NAME","DHSREGCO","DHSREGNA")) %>% 
+  left_join(shp_nfhs_2018 %>% 
+              distinct_at(vars(one_of("ADM1NAME","DHSREGCO","DHSREGNA")),.keep_all=TRUE) %>% 
+              dplyr::select(-DHSCLUST),
+            by = c("ADM1NAME","DHSREGCO","DHSREGNA")) %>% 
+  mutate(flag = "Imputed SHP 2018 data")
 
-shp_nfhs_2020 <- map2_dfr(psu_2020,1:735,
-                          function(x,y){
-                            
-                            x_df <- shp_nfhs[x,c("DHSCLUST","ADM1NAME","DHSREGCO","DHSREGNA","URBAN_RURA")];
-                            x_df$geometry <- NULL;
-                            y_df <- shp_2020[y,c("dtname","stname","stcode11","dtcode11","year_stat",
-                                                 "Dist_LGD","State_LGD","JID")];
-                            y_df$geometry <- NULL;
-                              bind_cols(x_df,y_df) %>% 
-                              return(.)
-                      
-                            
-                          })
-write_csv(shp_nfhs_2020,paste0("data/psu_on_map2020.csv"))
+
+missing_shp_nfhs$geometry <- NULL
+
+bind_rows(shp_nfhs_2018,
+          missing_shp_nfhs) %>% 
+  
+  write_csv(.,paste0("data/psu_on_map2018.csv"))
+
 
---
+++ code/n4d2005_psus within district map 2020.R
@@ -0,0 +1,43 @@
+# https://gis.stackexchange.com/questions/323698/counting-points-in-polygons-with-sf-package-of-r
+require(sf)
+require(purrr)
+require(tidyverse)
+
+
+path_2020 <- "C:/Cloud/OneDrive - Emory University/data/India Shapefiles/india_shp_2020-master/district"
+shp_2020 <-  read_sf(paste0(path_2020))
+psu_2020 <- st_intersects(shp_2020,shp_nfhs)
+
+shp_nfhs_2020 <- map2_dfr(psu_2020,1:735,
+                          function(x,y){
+                            
+                            x_df <- shp_nfhs[x,c("DHSCLUST","ADM1NAME","DHSREGCO","DHSREGNA","URBAN_RURA")];
+                            x_df$geometry <- NULL;
+                            y_df <- shp_2020[y,c("dtname","stname","stcode11","dtcode11","year_stat",
+                                                 "Dist_LGD","State_LGD","JID")];
+                            y_df$geometry <- NULL;
+                            bind_cols(x_df,y_df) %>% 
+                              return(.)
+                            
+                            
+                          })
+
+
+missing_shp_nfhs2020 <- anti_join(shp_nfhs %>% 
+                                    dplyr::select(one_of("DHSCLUST","ADM1NAME","DHSREGCO","DHSREGNA")),
+                                  shp_nfhs_2020,
+                                  by=c("DHSCLUST","ADM1NAME","DHSREGCO","DHSREGNA")) %>% 
+  left_join(shp_nfhs_2020 %>% 
+              distinct_at(vars(one_of("ADM1NAME","DHSREGCO","DHSREGNA")),.keep_all=TRUE) %>% 
+              dplyr::select(-DHSCLUST),
+            by = c("ADM1NAME","DHSREGCO","DHSREGNA")) %>% 
+  mutate(flag = "Imputed SHP 2020 data")
+
+
+missing_shp_nfhs2020$geometry <- NULL
+
+bind_rows(shp_nfhs_2020,
+          missing_shp_nfhs2020) %>% 
+  
+  write_csv(.,paste0("data/psu_on_map2020.csv"))
+
